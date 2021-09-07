e71291c8caa9a894bd166fb1356237278a0aef14
ruanramirez <ruanramirez01@gmail.com>
2021-09-07 03:35:48 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="pokedex">
+		<h1 class="text--yellow">Pokedéx</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'App',
+	};
+</script>
+
+<style lang="scss" scoped>
+#app {
+	#pokedex{
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		max-width: 1000px;
+		margin: 0 auto;
+		padding: 24px;
+	}
+}
+</style>
