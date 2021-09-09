cdd03904b9e704b3530ca0bf87436633e1c22211
ruanramirez <ruanramirez01@gmail.com>
2021-09-08 23:31:56 -0300

    add axios and first component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
 	<div id="pokedex">
-		<h1 class="text--yellow">Pokedéx</h1>
+		<Pokedex/>
 	</div>
 </template>
 
 <script>
+	import Pokedex from './components/Pokedex.vue';
+
 	export default {
 		name: 'App',
+		components: {
+			Pokedex,
+		},
 	};
 </script>
 
 <style lang="scss" scoped>
-#app {
-	#pokedex{
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-		max-width: 1000px;
-		margin: 0 auto;
-		padding: 24px;
-	}
+#pokedex{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: calc(100vh - 20px);
+	padding: 20px;
 }
 </style>
---
+++ src/components/Pokedex.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="pokedex">
+		<!-- <TopPanel/>
+		<LeftPanel class="pokedex--left"/>
+		<Divider/>
+		<RightPanel/> -->
+	</div>
+</template>
+
+<script>
+	// import TopPanel from './TopPanel/TopPanel.vue';
+	// import LeftPanel from './LeftPanel/LeftPanel.vue';
+	// import Divider from './Divider/Divider.vue';
+	// import Right from './RightPanel/RightPanel.vue';
+
+	export default {
+		name: 'Pokedex',
+		components: {
+			// TopPanel,
+			// LeftPanel,
+			// Divider,
+			// RightPanel,
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.pokedex {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 960px;
+		height: 100%;
+
+		@media (min-width: $viewport-medium) {
+			max-height: 700px;
+			flex-direction: row;
+		}
+
+		&--left,
+		&--right {
+			position: relative;
+			display: flex;
+			height: 50%;
+			perspective: 1700px;
+			transform-style: preserve-3d;
+
+			@media (min-width: $viewport-medium) {
+				height: 100%;
+			}
+		}
+	}
+</style>
